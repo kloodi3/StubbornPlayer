256b20bde7b0693baa4f05346b3f74e814d36e9b
kloodi3 <kmalrashdi@mail.csuchico.edu>
2018-03-01 18:07:57 -0800

    Small project

--- main.cpp
+++ main.cpp
@@ -6,6 +6,25 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+
+    int times;
+    int victory = 0;
+  cout<<"How many times do you want to play the game?"<<endl;    
+    cin>> times;
+      
+        for ( int i = 0 ; i<times ; i++ )
+ 
+{
+
+    game.newGame();
+    game.guessDoorC();
+    game.guessDoorC();
+
+      if ( game.isWinner() == 1) 
+      { 
+      victory++;
+      }
+ }
+          cout<<"congratulation,You have won "<<victory<<" times!"<<endl;
   return 0;
 }
